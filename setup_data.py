464c6a18d6307812860cbed475ee35b6779c6738
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2021-05-16 13:26:37 +0200

    add setup

---
+++ setup_data.py
@@ -0,0 +1,77 @@
+####################################################################################################
+#
+# KiCadRW - Python library to read/write KiCad Sexpr file format
+# Copyright (C) 2017 Fabrice Salvaire
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+####################################################################################################
+
+# Note: Still required for long_description
+
+####################################################################################################
+
+import os
+
+####################################################################################################
+
+def merge_include(src_lines, doc_path, included_rst_files=None):
+    if included_rst_files is None:
+        included_rst_files = {}
+    text = ''
+    for line in src_lines:
+        if line.startswith('.. include::'):
+            include_file_name = line.split('::')[-1].strip()
+            if include_file_name not in included_rst_files:
+                # print "include", include_file_name
+                with open(os.path.join(doc_path, include_file_name)) as f:
+                    included_rst_files[include_file_name] = True
+                    text += merge_include(f.readlines(), doc_path, included_rst_files)
+        else:
+            text += line
+    return text
+
+####################################################################################################
+
+# Utility function to read the README file.
+# Used for the long_description.
+def read_readme(file_name):
+
+    source_path = os.path.dirname(os.path.realpath(__file__))
+    if os.path.basename(source_path) == 'tools':
+        source_path = os.path.dirname(source_path)
+    elif 'build/bdist' in source_path:
+        source_path = source_path[:source_path.find('build/bdist')]
+    absolut_file_name = os.path.join(source_path, file_name)
+    doc_path = os.path.join(source_path, 'doc', 'sphinx', 'source')
+
+    # Read and merge includes
+    with open(absolut_file_name) as f:
+        lines = f.readlines()
+    text = merge_include(lines, doc_path)
+
+    return text
+
+####################################################################################################
+
+if not __file__.endswith('conf.py'):
+    long_description = read_readme('README.txt')
+else:
+    long_description = ''
+
+####################################################################################################
+
+setup_dict = dict(
+    long_description=long_description,
+)
